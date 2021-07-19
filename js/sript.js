a29a4c2961699afd54a6b76c37d0c10ca4376979
Alex <pryyda@yandex.ru>
2021-07-19 22:14:01 +0300

    firsttrial

---
+++ js/sript.js
@@ -0,0 +1,11 @@
+let name = 'Ksu';
+
+const header = document.querySelector('.about');
+const links = document.querySelectorAll('.link');
+console.log(links);
+
+function helloKsu() {
+    let hello = "Privet" + name;
+    console.log(hello);
+}
+helloKsu();
