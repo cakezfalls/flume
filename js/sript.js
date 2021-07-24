48a36fa22a7e4a23bc69621238d0916be95394ea
Alex <pryyda@yandex.ru>
2021-07-24 15:22:18 +0300

    slider menu

--- js/sript.js
+++ js/sript.js
@@ -1,11 +1,27 @@
-let name = 'Ksu';
+const tracklist = document.querySelectorAll('.album_tracklist');
+const blockStyle = document.querySelector('.block1_music');
+const blockStyle2 = document.querySelector('.block2_video');
 
-const header = document.querySelector('.about');
-const links = document.querySelectorAll('.link');
-console.log(links);
 
-function helloKsu() {
-    let hello = "Privet" + name;
-    console.log(hello);
+window.addEventListener('scroll', function() {
+    let scrollPos = window.scrollY;
+
+    if (scrollPos > 0) {
+        blockStyle.classList.add('green');
+    } else {
+        blockStyle.classList.remove('green');
+
+    }
+    if (scrollPos > 300) {
+        blockStyle2.classList.add('blue');
+    } else
+        blockStyle2.classList.remove('blue');
+
+});
+
+for (let trackList of tracklist) {
+    trackList.addEventListener('click', function() {
+        trackList.classList.toggle('open');
+
+    });
 }
-helloKsu();
